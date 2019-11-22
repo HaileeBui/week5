da468843be87457fd268b114a5fa85e4ac7ecfa6
Misty Bui <mishtybui@gmail.com>
2019-11-22 14:58:47 +0200

    updated 22.11

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+const catRoute = require('./routes/catRoute');
+const userRoute = require('./routes/userRoute');
+const cors = require ('cors');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('uploads'));
+
+
+app.use('/cat', catRoute);
+app.use('/user', userRoute);
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+'use strict';
+const userModel = require('../models/userModel');
+
+const users = userModel.users;
+
+const user_list_get = async (req, res) => {
+  const users = await userModel.getAllUsers();
+  await res.json(users);
+};
+
+const user_get = async (req, res) => {
+  const params = [req.params.id];
+  const user = await userModel.getUser(params);
+  await res.json(user[0]);
+};
+
+const user_create_post = async (req, res) => {
+  const params = [
+    req.body.name, 
+    req.body.email, 
+    req.body.passwd
+  ]; 
+  const response = await userModel.addUser(params);
+  const user = await userModel.getUser([response.insertId]);
+  res.json(user);
+};
+
+module.exports = {
+  user_list_get,
+  user_get,
+  user_create_post,
+};
